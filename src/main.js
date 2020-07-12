13fb81935da2b45b0eee81968af66c4b325d5f2e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-12 23:43:57 +0300

    Imports module card corrects render

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import {createTaskEditCardComponent} from './components/task-edit-card.js';
 import {createTaskCardComponent} from './components/task-card.js';
 import {createLoadMoreButtonComponent} from './components/load-more-button.js';
 
+import {generateCards} from './mock/card.js';
+
 const TASK_CARDS_AMOUNT = 3;
+const cards = generateCards(TASK_CARDS_AMOUNT);
 
 const pageMainElement = document.querySelector(`.main`);
 const pageMenuElement = pageMainElement.querySelector(`.main__control`);
@@ -19,11 +22,10 @@ renderComponent(pageMainElement, createSortComponent());
 const taskCardsElement = pageMainElement.querySelector(`.board__tasks`);
 const boardElement = pageMainElement.querySelector(`.board`);
 
+renderComponent(taskCardsElement, createTaskEditCardComponent(cards[0]));
 
-renderComponent(taskCardsElement, createTaskEditCardComponent());
-
-for (let i = 0; i < TASK_CARDS_AMOUNT; i++) {
-  renderComponent(taskCardsElement, createTaskCardComponent());
+for (let i = 0; i < cards.length; i++) {
+  renderComponent(taskCardsElement, createTaskCardComponent(cards[i]));
 };
 
 renderComponent(boardElement, createLoadMoreButtonComponent());
