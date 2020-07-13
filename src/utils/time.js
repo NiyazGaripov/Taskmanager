d8ce049c7294f37199f8d3dda8debeddd06a27d0
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-13 15:56:45 +0300

    Creates func get time

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,3 +1,10 @@
 const setTimeFormat = (value) => {
   return value < 10 ? `0${value}` : String(value);
 };
+
+export const getTime = (date) => {
+  const hours = setTimeFormat(date.getHours() % 24);
+  const minutes = setTimeFormat(date.getMinutes());
+
+  return `${hours}:${minutes}`;
+};
