e2660f73694990d3c394c463d5a81d3c9eb2ad97
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-16 18:11:55 +0300

    Adds random date, time, state for task edit card

--- src/components/task-edit-card.js
+++ src/components/task-edit-card.js
@@ -3,10 +3,12 @@ import {getTime} from './../utils/time.js';
 
 export const createTaskEditCardComponent = (amount) => {
   const {description, dueDate, color, repeatingDays} = amount;
+  const isExpired = dueDate instanceof Date && dueDate < Date.now();
+  const isDateShowing = !!dueDate;
   const repeatClass = `card--repeat`;
-  const deadlineClass = `card--deadline`;
-  const date = `23 September`;
-  const time = `16:15`;
+  const deadlineClass = isExpired ? `card--deadline` : ``;
+  const date = isDateShowing ? `${dueDate.getDate()} ${MONTH_NAMES[dueDate.getMonth()]}` : ``;
+  const time = isDateShowing ? getTime(dueDate) : ``;
 
   return (
     `<article class="card card--edit card--${color} ${repeatClass} ${deadlineClass}">
@@ -30,19 +32,21 @@ export const createTaskEditCardComponent = (amount) => {
             <div class="card__details">
               <div class="card__dates">
                 <button class="card__date-deadline-toggle" type="button">
-                  date: <span class="card__date-status">yes</span>
+                    date: <span class="card__date-status">${isDateShowing ? `yes` : `no`}</span>
                 </button>
-                <fieldset class="card__date-deadline">
-                  <label class="card__input-deadline-wrap">
-                    <input
-                      class="card__date"
-                      type="text"
-                      placeholder=""
-                      name="date"
-                      value="${date} ${time}"
-                    />
-                  </label>
-                </fieldset>
+                ${isDateShowing ?
+                  `<fieldset class="card__date-deadline">
+                    <label class="card__input-deadline-wrap">
+                      <input
+                        class="card__date"
+                        type="text"
+                        placeholder=""
+                        name="date"
+                        value="${date} ${time}"
+                      />
+                    </label>
+                  </fieldset>`
+                  : ``}
                 <button class="card__repeat-toggle" type="button">
                   repeat:<span class="card__repeat-status">yes</span>
                 </button>
