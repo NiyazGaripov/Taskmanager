da31aff07ffefe7ea837ffc60008566d934e912a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-18 22:54:55 +0300

    Creates func create filter item component

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,3 +1,20 @@
+const createFilterItemComponent = (filter, isChecked) => {
+  const {name, count} = filter;
+
+  return (
+    `<input
+      type="radio"
+      id="filter__${name}"
+      class="filter__input visually-hidden"
+      name="filter"
+      ${isChecked ? `checked` : ``}
+    />
+    <label for="filter__${name}" class="filter__label">
+      ${name} <span class="filter__${name}-count">${count}</span></label
+    >`
+  );
+};
+
 export const createFilterComponent = () => {
   return (
     `<section class="main__filter filter container">
