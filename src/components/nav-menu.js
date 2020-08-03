41536dd1a01d8a9f7a3b31426ded3d5c798ec8e2
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-03 21:25:08 +0300

    Creates class navigation menu

--- src/components/nav-menu.js
+++ src/components/nav-menu.js
@@ -32,3 +32,25 @@ const createNavigationMenuComponent = () => {
     </section>`
   );
 };
+
+export class NavigationMenu {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createNavigationMenuComponent();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
