264507b307b3057646b5695c1199ecf3dc36a6c5
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-06 22:13:25 +0300

    Creates map

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,3 +1,8 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
 export const renderComponent = (container, component, place = `beforeend`) => {
   container.insertAdjacentHTML(place, component);
 };
