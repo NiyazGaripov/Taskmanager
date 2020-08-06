8a9041e8698672680a5f8036545071f6eefc1ac1
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-06 22:03:10 +0300

    Imports func create element and creates class load more button

--- src/components/load-more-button.js
+++ src/components/load-more-button.js
@@ -1,5 +1,29 @@
-export const createLoadMoreButtonComponent = () => {
+import {createElement} from './../utils/render.js';
+
+const createLoadMoreButtonComponent = () => {
   return (
     `<button class="load-more" type="button">load more</button>`
   );
 };
+
+export class LoadMoreButton {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createLoadMoreButtonComponent();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
