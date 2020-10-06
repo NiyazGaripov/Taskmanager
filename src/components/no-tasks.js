6d30a66bdc4e677d83dff602c9a40adeff75b5a6
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-10-06 20:54:48 +0300

    Creates func createNoTaskListComponent

--- src/components/no-tasks.js
+++ src/components/no-tasks.js
@@ -1,1 +1,7 @@
 import {createElement} from './../utils/render.js';
+
+const createNoTaskListComponent = () => {
+  return (
+    `<p class="board__no-tasks">Click «ADD NEW TASK» in menu to create your first task</p>`
+  );
+};
