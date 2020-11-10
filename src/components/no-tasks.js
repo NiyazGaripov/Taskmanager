2e0296e56fba9877eb971ebb0a7f1ceaf61880de
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-11-10 21:22:53 +0300

    Inherits no tasks component from abstract component

--- src/components/no-tasks.js
+++ src/components/no-tasks.js
@@ -1,4 +1,4 @@
-import {createElement} from './../utils/render.js';
+import {AbstractComponent} from "./abstract-component";
 
 const createNoTaskListComponent = () => {
   return (
@@ -6,24 +6,8 @@ const createNoTaskListComponent = () => {
   );
 };
 
-export class NoTaskList {
-  constructor() {
-    this._element = null;
-  }
-
+export class NoTaskList extends AbstractComponent {
   getTemplate() {
     return createNoTaskListComponent();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
